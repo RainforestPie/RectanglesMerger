a30fc4d3205bfe0bff66b25cf1db2fc3f08e7008
RainforestPie <715498224@qq.com>
2023-10-12 20:52:00 +0800

    初步提交Demo工程

---
+++ RectanglesMerger/Assets/Scripts/Demo.cs
@@ -0,0 +1,30 @@
+﻿using GF2DarkZoneMapEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawResult();
+    }
+
+    private void DrawResult()
+    {
+        var boxColliders = GetComponentsInChildren<BoxCollider2D>();
+        var edges = RectanglesMerger.GetEdgesAfterMerge(boxColliders);
+        foreach (var edge in edges)
+        {
+            Debug.DrawLine(edge.startPoint, edge.endPoint, Color.red, 1);
+        }
+    }
+
+}
---
+++ RectanglesMerger/Assets/Scripts/RectanglesMerger.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GF2DarkZoneMapEditor
+{
+    /// <summary>
+    /// 将相邻矩形合并成一个多边形，绘制其边
+    /// 参考： https://stackoverflow.com/questions/13746284/merging-multiple-adjacent-rectangles-into-one-polygon
+    /// 基本思路：
+    /// 1. 去除共享顶点（最多保留1个）
+    /// 2. 分别按照x和y坐标排序
+    /// 3. 从x排序的点集中，解析出所有垂直方向上的边
+    /// 4. 从y排序的点集中，解析出所有水平方向上的边
+    /// </summary>
+    public static class RectanglesMerger
+    {
+        /// <summary>
+        /// 获取合并后的边
+        /// </summary>
+        /// <param name="boxColliders"></param>
+        /// <returns></returns>
+        public static List<(Vector2 startPoint, Vector2 endPoint)> GetEdgesAfterMerge(BoxCollider2D[] boxColliders)
+        {
+            var rectangles = GetRectangles(boxColliders);
+            var uniquePoints = GetUniquePoints(rectangles);
+
+            var pointsSortByX = uniquePoints.ToList();
+            pointsSortByX.Sort(new XThenYComparer());
+            var pointsSortByY = uniquePoints.ToList();
+            pointsSortByY.Sort(new YThenXComparer());
+
+            var edges = ResolveEdges(pointsSortByX, pointsSortByY);
+            return edges;
+        }
+
+        /// <summary>
+        /// 根据boxCollider，得到对应的矩形
+        /// </summary>
+        /// <param name="boxColliders"></param>
+        /// <returns></returns>
+        private static List<Vector2[]> GetRectangles(BoxCollider2D[] boxColliders)
+        {
+            // 获取所有boxColliders的矩形顶点，存为List<Vector2[]> rectangles
+            List<Vector2[]> rectangles = new List<Vector2[]>();
+
+            foreach (var boxCollider in boxColliders)
+            {
+                // Calculate the corner points of the rectangle
+                Vector2 p1 = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.min.y);
+                Vector2 p2 = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.min.y);
+                Vector2 p3 = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.max.y);
+                Vector2 p4 = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.max.y);
+
+                rectangles.Add(new Vector2[] { p1, p2, p3, p4 });
+            }
+
+            return rectangles;
+        }
+
+        /// <summary>
+        /// 矩形列表所有顶点集合（去除矩形的共享顶点。奇数个共享点，则保留1个；偶数个共享点，则全都去除）
+        /// </summary>
+        /// <param name="rectangles"></param>
+        private static List<Vector2> GetUniquePoints(List<Vector2[]> rectangles)
+        {
+            var uniquePoints = new List<Vector2>();
+
+            foreach (var rect in rectangles)
+            {
+                foreach (var point in rect)
+                {
+                    if (uniquePoints.Any(o => o == point))
+                    {
+                        uniquePoints.RemoveAll(o => o == point);
+                    }
+                    else
+                    {
+                        uniquePoints.Add(point);
+                    }
+                }
+            }
+            return uniquePoints;
+        }
+
+        /// <summary>
+        /// 解析合并后的多边形边
+        /// </summary>
+        /// <param name="pointsSortByX"></param>
+        /// <param name="pointsSortByY"></param>
+        /// <param name="edges"></param>
+        private static List<(Vector2, Vector2)> ResolveEdges(List<Vector2> pointsSortByX, List<Vector2> pointsSortByY)
+        {
+            List<(Vector2, Vector2)> edges = new List<(Vector2, Vector2)>();
+
+            int i = 0;
+            var pointCount = pointsSortByX.Count;
+            while (i < pointCount)
+            {
+                float currY = pointsSortByY[i].y;
+                while (i < pointCount && pointsSortByY[i].y == currY)
+                {
+                    var startPoint = pointsSortByY[i];
+                    var endPoint = pointsSortByY[i + 1];
+                    edges.Add((startPoint, endPoint));
+                    i += 2;
+                }
+            }
+
+            i = 0;
+            while (i < pointCount)
+            {
+                float currX = pointsSortByX[i].x;
+                while (i < pointCount && pointsSortByX[i].x == currX)
+                {
+                    var startPoint = pointsSortByX[i];
+                    var endPoint = pointsSortByX[i + 1];
+                    edges.Add((startPoint, endPoint));
+                    i += 2;
+                }
+            }
+            return edges;
+        }
+
+        /// <summary>
+        /// 先比较y坐标，再比较x坐标。通过 Mathf.Approximately 避免浮点数误差（——导致比较结果不符合预期）
+        /// </summary>
+        private class YThenXComparer : IComparer<Vector2>
+        {
+            public int Compare(Vector2 a, Vector2 b)
+            {
+                if (Mathf.Approximately(a.y, b.y))
+                {
+                    if (Mathf.Approximately(a.x, b.x))
+                    {
+                        return 0;
+                    }
+                    else if (a.x < b.x)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                else if (a.y < b.y)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 先比较x坐标，再比较y坐标。通过 Mathf.Approximately 避免浮点数误差（——导致比较结果不符合预期）
+        /// </summary>
+        private class XThenYComparer : IComparer<Vector2>
+        {
+            public int Compare(Vector2 a, Vector2 b)
+            {
+                if (Mathf.Approximately(a.x, b.x))
+                {
+                    if (Mathf.Approximately(a.y, b.y))
+                    {
+                        return 0;
+                    }
+                    else if (a.y < b.y)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                else if (a.x < b.x)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+}
